fbd65b880be911a4806635c696a7b664c29af587
Shawn408 <jokershawn@DESKTOP-04IPIDJ>
2022-03-09 16:22:17 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [review, setReview] = useState([]);
+  const [delreview, setDelete] = useState([]);
   useEffect(() => {
     fetch("/edit", { method: "POST" }).then((response) => 
       response.json().then((data) => { 
@@ -10,16 +11,30 @@ function App() {
       })
     );
   })
+function handleDelete(id) {
+  fetch(`/edit/${id}`, { method: "POST" }).then((response) => 
+    response.json().then((data) => { 
+      setDelete(data.delreview);
+    })
+  );
+}
 
+function Button(props) {
   return (
+    <button onClick={() => handleDelete(props.id)}>Delete</button>
+  );
+}
+ 
+return (
     <div className="App">
       <h1>Your Reviews:</h1>
       <table>
         {review && review.map((review) => 
           <tr>
-            <tb>{review.movie_id}</tb>
-            <tb>{review.rating}</tb>
+            <tb>{review.movie_id}</tb>&nbsp;
+            <tb>{review.rating}</tb>&nbsp;
             <tb>{review.comment}</tb>
+            <Button id={review.id} />
           </tr>
           )}
       </table>
